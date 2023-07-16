40833efef69d7c7d66c8a80d7394a8f8015226fa
Tom Irimescu <tom@irimescu.com>
2023-07-16 10:59:25 -0400

    Set up initial theme, modify index title, description, font

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,15 +2,18 @@ import type { V2_MetaFunction } from '@remix-run/node';
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: 'New Remix App' },
-    { name: 'description', content: 'Welcome to Remix!' },
+    { title: 'Batman App' },
+    {
+      name: 'description',
+      content: 'Remix Typescript Prisma Mongodb Tailwind',
+    },
   ];
 };
 
 export default function Index() {
   return (
     <div className=' flex flex-row justify-center my-48'>
-      <h1 className='text-3xl text-blue-500 font-bold'>Hello world!</h1>
+      <h1 className='text-3xl text-pink font-bold'>Hello world!</h1>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,23 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      transparent: 'transparent',
+      darkblue: '#1a237e',
+      blue: '#303f9f',
+      mediumblue: '#3949ab',
+      lightblue: '#3f51b5',
+      pink: '#e91e62',
+      'gray-dark': '#273444',
+      gray: '#8492a6',
+      'gray-light': '#d3dce6',
+    },
   },
   plugins: [require('@headlessui/tailwindcss')],
 } satisfies Config;
